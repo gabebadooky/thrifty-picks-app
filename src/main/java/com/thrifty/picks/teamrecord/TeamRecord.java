d41e66cea17cef5465db1dd6b8a1e7200a42fac4
Gabriel Baduqui <baduquig@gmail.com>
2026-05-15 14:00:33 -0700

    Define TeamRecord IdClass and entity class

---
+++ src/main/java/com/thrifty/picks/teamrecord/TeamRecord.java
@@ -0,0 +1,51 @@
+package com.thrifty.picks.teamrecord;
+
+import com.thrifty.picks.team.Team;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.OffsetDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(
+		name = "team_record",
+		schema = "pickem",
+		indexes = @Index(name = "idx_team_record_team", columnList = "team"))
+@IdClass(TeamRecordId.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TeamRecord {
+
+	@Id
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "team", nullable = false)
+	private Team team;
+
+	@Id
+	@Column(name = "record_type", length = 10, nullable = false)
+	private String recordType;
+
+	private Short wins;
+
+	private Short losses;
+
+	private Short ties;
+
+	@UpdateTimestamp
+	@Column(name = "updated_at", nullable = false)
+	private OffsetDateTime updatedAt;
+}
---
+++ src/main/java/com/thrifty/picks/teamrecord/TeamRecordId.java
@@ -0,0 +1,19 @@
+package com.thrifty.picks.teamrecord;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class TeamRecordId implements Serializable {
+
+	private String team;
+	private String recordType;
+}
